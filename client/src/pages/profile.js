31d22ca9b3cb04d67f09932fabfc3a96cae7a922
tiptondt1998 <dylan.tipton@live.com>
2021-07-28 01:40:48 -0400

    updated main page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,6 +1,21 @@
+import "./styles.css";
 function profile(){
     return(
-        <section><p>test</p></section>
+        
+        <section>
+            <ul>
+                <li> <a>Videos</a> </li>
+                <li> <a>Merch Store</a> </li>
+                <li> <a>Application</a> </li>
+            </ul>
+            <p id="meet">Meet the TUR Family</p>
+            <h1>Jay</h1>
+            <p id="meet_jay">asdfghgfdsdfgfdsdfgfdsdfgfdsdfgfdsdfgfdsdfgfdsdfgfdwqwsdfgfdewedfghgfdsdfvbhtredfghjtrf</p>
+            <h1>Lola</h1>
+            <p id="meet_jay">asdfghgfdsdfgfdsdfgfdsdfgfdsdfgfdsdfgfdsdfgfdsdfgfdwqwsdfgfdewedfghgfdsdfvbhtredfghjtrf</p>
+            <nav />
+        </section>
+
     )
 }
 export default profile;
