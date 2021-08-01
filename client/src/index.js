db71e3c13fb227f611c90f50236f319d13e4b021
tiptondt1998 <dylan.tipton@live.com>
2021-08-01 13:54:45 -0400

    resolving deployment issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../public/index.html';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
